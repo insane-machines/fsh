addec3d56b4fce20f96c9abe0bd7d7c0e42b9ff8
insane-machines <forshnikita@gmail.com>
2026-07-21 21:24:57 +0300

    Created Cargo project

---
+++ gradforge/rust/engine/target/debug/build/libc-da11e2e266c88c2b/build_script_build-da11e2e266c88c2b.d
@@ -0,0 +1,5 @@
+/home/nikita/projects/gradforge/rust/engine/target/debug/build/libc-da11e2e266c88c2b/build_script_build-da11e2e266c88c2b.d: /home/nikita/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.188/build.rs
+
+/home/nikita/projects/gradforge/rust/engine/target/debug/build/libc-da11e2e266c88c2b/build_script_build-da11e2e266c88c2b: /home/nikita/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.188/build.rs
+
+/home/nikita/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.188/build.rs:
