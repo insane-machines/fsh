79427b84e2e655f0b5a75216069c26c5556b1f96
insane-machines <forshnikita@gmail.com>
2026-07-21 21:20:34 +0300

    Modified type checking in BackendManager with typing.Optional

--- gradforge/gradforge/backend/backend_manager.py
+++ gradforge/gradforge/backend/backend_manager.py
@@ -1,7 +1,7 @@
 from .backend import Backend
-
+from typing import Optional
 class BackendManager():
-    _backend = None
+    _backend: Optional[Backend] = None
 
     @staticmethod
     def set_backend(backend_adapter: Backend):
