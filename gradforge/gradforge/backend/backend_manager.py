cdd0f602bb0099bf2931b5994f01639c4e27b879
insane-machines <forshnikita@gmail.com>
2026-07-21 14:35:50 +0300

    All library has renamed to GradForge

---
+++ gradforge/gradforge/backend/backend_manager.py
@@ -0,0 +1,18 @@
+from .backend import Backend
+
+class BackendManager():
+    _backend = None
+
+    @staticmethod
+    def set_backend(backend_adapter: Backend):
+        if backend_adapter is None:
+            raise ValueError("Backend adapter cannot be None")
+        BackendManager._backend = backend_adapter
+
+    @staticmethod
+    def get_backend():
+        if BackendManager._backend is None:
+            raise RuntimeError("Backend is not initialized yet. \
+                               Please set backend with BackendManager.set(<your backend>)")
+        return BackendManager._backend
+         
