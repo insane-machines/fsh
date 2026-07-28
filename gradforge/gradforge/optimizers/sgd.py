15a8a0fd45eafb2810cfc4041c0544ae6aa03fb1
insane-machines <forshnikita@gmail.com>
2026-07-28 20:11:40 +0300

    Changed optimizer responsibility

--- gradforge/gradforge/optimizers/sgd.py
+++ gradforge/gradforge/optimizers/sgd.py
@@ -4,6 +4,6 @@ class SGD(Optimizer):
     def __init__(self, learning_rate=0.001) -> None:
         super().__init__(learning_rate)
 
-    def step(self):
-        for param in self.params:
+    def step(self, params):
+        for param in params:
             param.data -= param.grad * self.learning_rate
