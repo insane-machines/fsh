bf313bb919b73576a9368b18987fb0b87ce3dbe9
insane-machines <forshnikita@gmail.com>
2026-08-24 15:45:54 +0300

    Tests changed

--- gradforge/gradforge/_tests/test_tensor.py
+++ gradforge/gradforge/_tests/test_tensor.py
@@ -1,28 +1,41 @@
 from gradforge.core.tensor import Tensor
 from ..optimizers.sgd import SGD
 from ..autograd.standard_engine import StandardEngine
+
 class Linear:
-    def train(self, x: Tensor, y: Tensor):
-        w = Tensor(x.backend.array_zeros((x.data.shape[1], y.data.shape[1])), requires_grad=True)
-        print(w.data.shape)
+    def __init__(self, x: Tensor, y: Tensor):
+        self.w = Tensor([], requires_grad=True)
+        self.x = x
+        self.y = y
+
+    @property
+    def params(self) -> list[Tensor]:
+        return [attr_val\
+                 for attr_val in self.__dict__.values()\
+                      if attr_val.requires_grad]
+    
+    def train(self, epochs):
+        self.w.data = self.x.backend.array_zeros((self.x.data.shape[1], y.data.shape[1]))
         engine = StandardEngine()
         optimizer = SGD(learning_rate=1e-3)
-        for i in range(200):
-            y_pred =  x @ w 
-            error = y - y_pred
-            loss = error ** 2
+
+        for epoch in range(epochs):
+            y_pred  =  self.x @ self.w
+            error   = self.y - y_pred
+            loss    = error ** 2
             engine.backward(loss)
-            optimizer.step([w])
-            optimizer.zero_grad([w])
-            print(f"Weight: {w.data}")
+            optimizer.step(self.params)
+            optimizer.zero_grad(self.params)
 
-        res = x.data @ w.data
-        assert res.data.shape == y.data.shape
+        print(self.x.data @ self.w.data)
 
-x = Tensor([[4, 8]])
-y = Tensor([[8, 16]])
-model = Linear()
-print(x.data.shape, y.data.shape)
-model.train(x, y)
+x = Tensor([[4], [8], [9]])
+y = Tensor([[8], [16], [18]])
+test = Tensor([[9], [10]])
+assert x.data.shape == y.data.shape
 
+model = Linear(x, y)
+model.train(epochs=10000)
 
+print(test.data @ model.w.data)
+print(model.w.data.shape)
